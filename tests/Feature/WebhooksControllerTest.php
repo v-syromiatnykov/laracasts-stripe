64bfa435c53363a36172214e6bc358f771185095
Syromiatnykov Vitalii <corsarstl@gmail.com>
2018-04-26 13:30:41 +0300

    Lesson 11. 'Testing Stripe Webhooks'

--- tests/Feature/WebhooksControllerTest.php
+++ tests/Feature/WebhooksControllerTest.php
@@ -3,15 +3,38 @@
 namespace Tests\Feature;
 
 use App\Http\Controllers\WebhooksController;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class WebhooksControllerTest extends TestCase
 {
+    use RefreshDatabase;
+
     /** @test */
     public function it_converts_a_stripe_event_name_to_a_method_name()
     {
         $name = (new WebhooksController)->eventToMethod('customer.subscription.deleted');
 
         $this->assertEquals('whenCustomerSubscriptionDeleted', $name);
+    }
+    
+    /** @test */
+    public function it_deactivates_a_users_subscription_if_deleted_on_stripes_end()
+    {
+        $user = factory('App\User')->create([
+            'stripe_active' => 1,
+            'stripe_id' => 'fake_stripe_id'
+        ]);
+
+        $this->post('stripe/webhook', [
+            'type' => 'customer.subscription.deleted',
+            'data' => [
+                'object' => [
+                    'customer' => $user->stripe_id
+                ]
+            ]
+        ]);
+
+        $this->assertFalse($user->fresh()->isSubscribed());
     }
 }
