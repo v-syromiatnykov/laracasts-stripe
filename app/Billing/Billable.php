c115968edbac065ad12ce804c30a7ea61abc528b
Syromiatnykov Vitalii <corsarstl@gmail.com>
2018-04-26 15:02:07 +0300

    Lesson 13. 'A Billable Trait'

---
+++ app/Billing/Billable.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Billing;
+
+use App\Subscription;
+use Carbon\Carbon;
+
+trait Billable
+{
+    public static function byStripeId($stripeId)
+    {
+        return static::where('stripe_id', $stripeId)->firstOrFail();
+    }
+
+    public function setStripeSubscription($id)
+    {
+        $this->stripe_subscription = $id;
+    }
+
+    public function activate($customerId, $subscriptionId)
+    {
+        return $this->update([
+            'stripe_id' => $customerId,
+            'stripe_active' => true,
+            'stripe_subscription' => $subscriptionId,
+            'subscription_end_at' => 0
+        ]);
+    }
+
+    public function deactivate()
+    {
+        return $this->update([
+            'stripe_active' => false,
+            'subscription_end_at' => Carbon::now()
+        ]);
+    }
+
+    public function subscription()
+    {
+        return new Subscription($this);
+    }
+
+    public function isSubscribed()
+    {
+        return !! $this->stripe_active;
+    }
+}
+
--- app/User.php
+++ app/User.php
@@ -2,60 +2,17 @@
 
 namespace App;
 
-use Carbon\Carbon;
-use Illuminate\Notifications\Notifiable;
+use App\Billing\Billable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable, Billable;
 
     protected $guarded = [];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
         'password', 'remember_token',
     ];
-
-    public static function byStripeId($stripeId)
-    {
-        return static::where('stripe_id', $stripeId)->firstOrFail();
-    }
-
-    public function setStripeSubscription($id)
-    {
-        $this->stripe_subscription = $id;
-    }
-    
-    public function activate($customerId, $subscriptionId)
-    {
-        return $this->update([
-            'stripe_id' => $customerId,
-            'stripe_active' => true,
-            'stripe_subscription' => $subscriptionId,
-            'subscription_end_at' => 0
-        ]);
-    }
-
-    public function deactivate()
-    {
-        return $this->update([
-            'stripe_active' => false,
-            'subscription_end_at' => Carbon::now()
-        ]);
-    }
-
-    public function subscription()
-    {
-        return new Subscription($this);
-    }
-
-    public function isSubscribed()
-    {
-        return !! $this->stripe_active;
-    }
 }
